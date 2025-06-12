341abf9fd09a7e04d86e605b501edde0d7d359de
Maysalopess <maysalopess03@outlook.com>
2025-06-12 17:41:28 -0300

    ✨Subindo o padrão Builder

---
+++ Builder/Builder/BuilderProblema/PDF/GeradorPDFDirector.cs
@@ -0,0 +1,44 @@
+﻿namespace BuilderProblema.PDF;
+
+public class GeradorPDFDirector
+{
+    private IGeradorPDFBuilder _geradorPDFBuilder;
+
+    public GeradorPDFDirector(IGeradorPDFBuilder builder)
+    {
+        _geradorPDFBuilder = builder;
+    }
+
+    public void SetGeradorPDFBuilder(IGeradorPDFBuilder builder)
+    {
+        _geradorPDFBuilder = builder;
+    }
+
+    public void CriarGeradorPDF()
+    {
+        _geradorPDFBuilder.SetPageConfiguration();
+        _geradorPDFBuilder.SetMargin();
+        _geradorPDFBuilder.SetHeader();
+        _geradorPDFBuilder.SetFooter();
+    }
+
+    public void CriarGeradorPDFNoHeader()
+    {
+        _geradorPDFBuilder.SetPageConfiguration();
+        _geradorPDFBuilder.SetMargin();
+        _geradorPDFBuilder.SetFooter();
+    }
+
+    public void CriarGeradorPDFNoFooter()
+    {
+        _geradorPDFBuilder.SetPageConfiguration();
+        _geradorPDFBuilder.SetMargin();
+        _geradorPDFBuilder.SetHeader();
+    }
+
+    public void CriarGeradorPDFNoHeaderNoFooter()
+    {
+        _geradorPDFBuilder.SetPageConfiguration();
+        _geradorPDFBuilder.SetMargin();
+    }
+}
---
+++ Builder/Builder/BuilderProblema/PDF/IGeradorPDFBuilder.cs
@@ -0,0 +1,10 @@
+﻿namespace BuilderProblema.PDF;
+
+public interface IGeradorPDFBuilder
+{
+    public GeradorPDF GetGeradorPDF();
+    public void SetPageConfiguration();
+    public void SetMargin();
+    public void SetHeader();
+    public void SetFooter();
+}
